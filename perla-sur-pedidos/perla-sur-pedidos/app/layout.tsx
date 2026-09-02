b850d4691fc4d03328314c190e38c5c9ac827917
somosperlasur <somosperlasur@gmail.com>
2026-09-02 12:01:21 -0500

    Add files via upload

--- perla-sur-pedidos/perla-sur-pedidos/app/layout.tsx
+++ perla-sur-pedidos/perla-sur-pedidos/app/layout.tsx
@@ -1,4 +1,10 @@
 import type { Metadata } from "next";
+import "@fontsource/fraunces/500.css";
+import "@fontsource/fraunces/600.css";
+import "@fontsource/fraunces/700.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
